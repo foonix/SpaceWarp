3c141574a943ca8c48e79065e2d91460d72f5cb7
Lexi <arallen3660@gmail.com>
2025-02-14 12:54:57 -0500

    Change path to mirror both playmode and editor

--- Runtime/Core/API/CommonPaths.cs
+++ Runtime/Core/API/CommonPaths.cs
@@ -8,13 +8,13 @@ namespace SpaceWarp.API;
 [PublicAPI]
 public static class CommonPaths
 {
-#if UNITY_EDITOR
-    public const string MODS_FOLDER = "Assets/Mods";
-    public const string DISABLED_PLUGINS = "Assets/disabled_plugins.cfg";
-    public const string HASH_LOCATION = "Assets/mods_list_hash.txt";
-#else
+// #if UNITY_EDITOR
+//     public const string MODS_FOLDER = "Assets/Mods";
+//     public const string DISABLED_PLUGINS = "Assets/disabled_plugins.cfg";
+//     public const string HASH_LOCATION = "Assets/mods_list_hash.txt";
+// #else
     public const string MODS_FOLDER = "./mods/";
     public const string DISABLED_PLUGINS = "disabled_plugins.cfg";
     public const string HASH_LOCATION = "mods_list_hash.txt";
-#endif
+// #endif
 }
